5f76b5119d82246c130123fbbe8df10ad79593dd
Tom Roida <tom@roida.de>
2020-11-21 02:14:37 +0100

    modify top level backports/Kconfig with backports identity
    (took 0.19 seconds)
    
    Change-Id: Id0d7f70de789353cfab96e9a8cdaf5c768cc6022

---
+++ backports/include/crypto/pkcs7.h
@@ -0,0 +1,6 @@
+/* Automatically created during backport process */
+#ifndef CONFIG_BACKPORT_BPAUTO_PKCS7
+#include_next <crypto/pkcs7.h>
+#else
+#include <crypto/backport-pkcs7.h>
+#endif /* CONFIG_BACKPORT_BPAUTO_PKCS7 */
